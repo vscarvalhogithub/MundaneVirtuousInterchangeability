89ef9817fac80d518b6e65446c0119c1b77a7e01
ViniciusCarva35 <22045171-ViniciusCarva35@users.noreply.replit.com>
2023-04-09 22:00:12 +0000

    Correção do erro no cadastro. Após o cadastro, loga automaticamente.

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,30 @@ import Login from './Login';
 import DataEntry from './DataEntry';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 
-
-
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleLogin = async (email, password) => {
-  try {
-    // Autenticar o usuário com Firebase
-    await signInWithEmailAndPassword(auth, email, password);
-    // Se o login for bem-sucedido, atualize o estado isLoggedIn
+    try {
+      // Autenticar o usuário com Firebase
+      await signInWithEmailAndPassword(auth, email, password);
+      // Se o login for bem-sucedido, atualize o estado isLoggedIn
+      setIsLoggedIn(true);
+    } catch (error) {
+      console.error('Erro ao efetuar login:', error.message);
+      // Aqui você pode lidar com erros de login, por exemplo, mostrar uma mensagem de erro para o usuário
+    }
+  };
+
+  const handleSignUp = () => {
+    // Atualize o estado isLoggedIn
     setIsLoggedIn(true);
-  } catch (error) {
-    console.error('Erro ao efetuar login:', error.message);
-    // Aqui você pode lidar com erros de login, por exemplo, mostrar uma mensagem de erro para o usuário
-  }
-};
+  };
 
-
- return (
+  return (
     <div>
       {!isLoggedIn ? (
-        <Login onLogin={handleLogin} />
+        <Login onLogin={handleLogin} onSignUp={handleSignUp} />
       ) : (
         <DataEntry />
       )}
