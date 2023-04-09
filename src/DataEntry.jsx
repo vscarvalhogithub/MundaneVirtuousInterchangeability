d4c6d60044b3938e0daf289673d1683c82ca9566
Vinícius Carvalho <112362239+vscarvalhogithub@users.noreply.github.com>
2023-04-09 18:36:08 +0000

    o componente irá preencher os campos de entrada com os dados retornados

--- src/DataEntry.jsx
+++ src/DataEntry.jsx
@@ -4,16 +4,25 @@ import { collection, addDoc } from 'firebase/firestore';
 import api from './api';
 
 function DataEntry() {
-  const [value, setValue] = useState('');
+  const [name, setName] = useState('');
   const [cpf, setCpf] = useState('');
   const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await api.get('/users/2');
-        console.log(response.data);
+        const data = response.data;
+
+        // Verifique se os dados retornados pela API estão no formato correto
+        console.log("Data from API:", data);
+
+        setName(data.name || ''); // Adicione '||' para evitar 'undefined'
+        setCpf(data.cpf || '');
+        setPhone(data.phone || '');
+        setEmail(data.email || '');
       } catch (error) {
         console.error('Erro ao buscar dados da API:', error.message);
       }
@@ -26,10 +35,11 @@ function DataEntry() {
     event.preventDefault();
     try {
       // Salve os dados no Firestore
-      await addDoc(collection(db, 'data'), { value, cpf, phone });
-      setValue('');
+      await addDoc(collection(db, 'data'), { name, cpf, phone, email });
+      setName('');
       setCpf('');
       setPhone('');
+      setEmail('');
       setMessage('Dados salvos com sucesso!');
     } catch (error) {
       console.error('Erro ao salvar dados:', error.message);
@@ -45,8 +55,8 @@ function DataEntry() {
           Nome:
           <input
             type="text"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             placeholder="Insira seu nome aqui"
             required
           />
@@ -68,6 +78,16 @@ function DataEntry() {
             value={phone}
             onChange={(e) => setPhone(e.target.value)}
             placeholder="Insira seu telefone"
+            required
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Insira seu e-mail"
             required
           />
         </label>
